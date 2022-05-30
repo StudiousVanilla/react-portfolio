a7cf3b4f1927d520834ac423724c3f21db0452ba
Oisín Byrne <obyrne@cartrawler.com>
2022-05-30 18:37:08 +0100

    merge test

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -3,7 +3,7 @@ const ContactForm = () => {
     <div className="text-slate-300">
       <form className="flex flex-col">
         <label for="fname">Name:</label>
-        <input type="text" id="name" name="name" />
+        <input type="text" id="name" name="name" className="border-gray-300 focus:border-red-400"/>
         <label for="lname">Last name:</label>
         <input type="text" id="lname" name="lname" />
       </form>
