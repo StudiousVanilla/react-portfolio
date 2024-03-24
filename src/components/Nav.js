36dc6d6381fb8752a7acf72901d1279fcf212d3b
Oisín Byrne <byrne.ois@gmail.com>
2024-03-24 16:09:54 +0000

    basic wedding mockup 2

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,17 @@
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 const Nav = () => {
+
+  const location = useLocation();
+
+  // Check if the current pathname is "/wedding"
+  const hideNav = location.pathname === '/wedding';
+
+  // Return null if hideNav is true, effectively hiding the Nav component
+  if (hideNav) {
+    return null;
+  }
+
   return (
     <div className="w-full mb-2 py-6 lg:pt-8 font-semibold text-slate-500 z-40 absolute">
       <div className="flex justify-end">
