1eaaca77e82b9a3879a50ab4c406bb9f68e87ae4
Oisín Byrne <byrne.ois@gmail.com>
2024-03-24 18:58:02 +0000

    removed google maps and made mobile view for page 1

--- src/pages/WeddingPage.js
+++ src/pages/WeddingPage.js
@@ -5,43 +5,31 @@ import info from '../assests/weddingImages/info.png';
 const ContactPage = () => {
   return (
     <div className="w-full h-screen overflow-x-hidden snap-mandatory snap-y scroll-smooth">
-    
-      <div className="h-screen w-full flex flex-col justify-center items-center bg-[#1baa9d] snap-start scroll-smooth">
+
+        {/* 1st page */}
+      <div className="h-screen w-full flex flex-col md:justify-center items-center bg-[#1baa9d] snap-start scroll-smooth">
         
-        <div className="w-full px-20 flex justify-center items-center bg-[#1baa9d]">
+        <div className="md:w-full md:px-20 md:mt-0 md:mb-0 flex justify-center items-center bg-[#1baa9d] w-2/3 mt-16 mb-4">
           <img src={title} alt="page title" className="my-4" />
         </div>
 
-        <div className="w-full flex justify-center px-20">
+        <div className="w-full flex justify-center md:px-20">
           <img
             src={collage}
             alt="Collage of Aoife & Oisín"
-            className=" w-[96%] bg-slate-200"
+            className="md:w-[96%] w-full bg-slate-200"
           />
         </div>
 
       </div>
 
+      {/* 2nd page */}
+
       <div className="h-screen snap-start scroll-smooth flex flex-col justify-center items-center md:flex-row pt-1">
 
         {/* Wedding info */}
-        <div className="w-full h-2/3 mb-2 md:h-full md:w-3/5">
+        <div className="w-full">
           <img src={info} alt="Wedding info" className="bg-slate-200 w-full h-full" />
-        </div>
-
-         {/* google map */}
-        <div className="w-full h-1/3 md:w-2/5 flex justify-center items-center bg-white">
-          {/* google */}
-          <div className="w-2/3 h-full">
-            <iframe
-              width="100%"
-              height="100%"
-              src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Foley's%20Bar,%201%20Merrion%20Row,%20Dublin%202,%20D02%20HY07,%20Ireland+(Foleys%20Bar)&amp;t=&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" title="Google map"
-            >
-              <a href="https://www.gps.ie/">gps vehicle tracker</a>
-            </iframe>
-          </div>
-          {/* google */}
         </div>
 
       </div>
