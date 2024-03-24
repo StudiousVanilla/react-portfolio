9100b963cb3e63692fa4d5babfd5cb8dd7cab31f
Oisín Byrne <byrne.ois@gmail.com>
2024-03-24 14:49:26 +0000

    basic wedding mockup

--- src/pages/WeddingPage.js
+++ src/pages/WeddingPage.js
@@ -1,14 +1,32 @@
+import collage from '../assests/weddingImages/collage.png';
+import title from '../assests/weddingImages/title.png';
+import info from '../assests/weddingImages/info.png';
 
 const ContactPage = () => {
   return (
-    <div className="w-full h-screen overflow-x-hidden snap-mandatory snap-y scroll-smooth bg-slate-900">
+    <div className="w-full h-screen overflow-x-hidden snap-mandatory snap-y scroll-smooth bg-white">
 
-    <p className="w-full text-red-400" >
-      wedding
-    </p>
+      <div className="w-full px-20 mt-10 flex justify-center items-center">
+        <img
+          src={title}
+          alt="Collage of Aoife & Oisín"
+          className="bg-slate-200"
+        />
+      </div>
 
+      <div className="w-full px-20 mt-10">
+        <img
+          src={collage}
+          alt="Collage of Aoife & Oisín"
+          className=" w-full bg-slate-200"
+        />
+      </div>
 
-
+      <img
+        src={info}
+        alt="Collage of Aoife & Oisín"
+        className="bg-slate-200 h-2/3 w-2/3"
+      />
     </div>
   );
 };
